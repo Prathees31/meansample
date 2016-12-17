46dd88cff381be19e445a39a13e3fb98c671622e
Prathees31 <prathees.g.s@gmail.com>
2016-12-17 16:33:45 +0530

    thirdversion

---
+++ public/app/app.routes.js
@@ -0,0 +1,40 @@
+angular.module('appRoutes', ['ngRoute'])
+
+
+.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		.when('/', {
+			templateUrl: 'app/views/pages/home.html',
+			controller: 'MainController',
+			controllerAs: 'main'
+		})
+		.when('/login', {
+			templateUrl: 'app/views/pages/login.html'
+		})
+		.when('/signup', {
+			templateUrl: 'app/views/pages/signup.html'
+		})
+		.when('/boxed-layout', {
+			templateUrl: 'app/views/pages/boxed-layout.html'
+		})
+		.when('/iconic-leftbar', {
+			templateUrl: 'app/views/pages/iconic-leftbar.html'
+		})
+
+		.when('/allStories', {
+			templateUrl: 'app/views/pages/allStories.html',
+			controller: 'AllStoriesController',
+			controllerAs: 'story',
+			resolve: {
+				stories: function(Story) {
+					return Story.allStories();
+				}
+			}
+
+		})
+
+	$locationProvider.html5Mode(true);
+
+})
